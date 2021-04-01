29be8ee7142b29ac9393e72010006ebb3c16bdef
Lytexx <28214667+Lytexx@users.noreply.github.com>
2021-04-01 09:04:06 +0200

    Update postprocess.ps1

--- scripts/Windows/postprocess.ps1
+++ scripts/Windows/postprocess.ps1
@@ -8,8 +8,8 @@ Run this script while the stream is live.
 Press enter after the stream is saved.
 #>
 $stream_url = $args[0]
-$filename = youtube-dl --write-description --skip-download -o "[%(uploader)s][%(upload_date)s] %(title)s (%(id)s)" --get-filename $stream_url
-youtube-dl --write-thumbnail --write-description --skip-download -o stream $stream_url
-Read-Host -Prompt 'Press [Enter] to continue. Make sure Streamlink has finished recording the stream.'
+$filename = yt-dlp --write-description --skip-download -o "[%(uploader)s][%(upload_date)s] %(title)s (%(id)s)" --get-filename $stream_url
+yt-dlp --write-thumbnail --write-description --skip-download -o stream $stream_url
+Read-Host -Prompt 'Press [Enter] to continue. Make sure ytarchive has finished recording the stream.'
 $description = [IO.File]::ReadAllText(".\stream.description")
 ffmpeg -i .\stream.mp4 -i .\stream.jpg -map 1 -map 0 -c copy -disposition:0 attached_pic -metadata comment=$description $($filename + ".mp4")
